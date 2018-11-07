d22f5f96f9eee293bd6b90ef11a01084586ff237
RashidMall <mr.mallaev@gmail.com>
2018-11-07 16:05:15 +0100

    Init

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome To Alumnis';
+        /* return view('pages.index', compact('title')); */
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        $title = 'About Alumnis';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services(){
+        $title = 'Alumnis Services';
+        return view('pages.services')->with('title', $title);
+    }
+}
